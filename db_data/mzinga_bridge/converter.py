f94b6d11b4fd1fc64fa76419972b69fce50d37c3
FGaragnani <filippogaragnani1@gmail.com>
2025-02-18 10:50:34 +0100

    Converter done!

--- db_data/mzinga_bridge/converter.py
+++ db_data/mzinga_bridge/converter.py
@@ -10,20 +10,65 @@ class Piece:
         
     def change_position(self, new_position: tuple[int, int, int]):
         self.position = new_position
+        
+    def get_position(self) -> tuple[int, int, int]:
+        return self.position
+        
 
 class Collector:
     
-    def __init__(self):
-        self.array = []
+    def __init__(self, first_move: str):
+        first_piece = Piece(first_move[0], (0, 0, 0), first_move[1:])
+        self.array: list[Piece] = [first_piece]
+        self.position_chars: list[str] = [
+            "-", "\\", "/"
+        ]
         
     def add_piece(self, piece: Piece):
         self.array.append(piece)
         
-    def get_piece(self, id: str):
+    def get_piece(self, id: str, color: str) -> Piece:
         for piece in self.array:
-            if piece.id == id:
+            if piece.id == id and piece.color == color:
                 return piece
         return None
+    
+    def manage_piece(self, mzinga_str: str):
+        first_piece, second_piece = mzinga_str.split()[0:2]
+        first_color = first_piece[0]
+        first_name = first_piece[1:]
+        first_piece: Piece = self.get_piece(first_name, first_color)
+        if first_piece is None:
+            first_piece = Piece(first_color, (0, 0, 0), first_name)
+            self.add_piece(first_piece)
+        
+        if second_piece[0] in self.position_chars:
+            second_color = second_piece[1]
+            second_name = second_piece[2:]
+            second_position = self.get_piece(second_name, second_color).get_position()
+            position_change = (-2, 0, 0) if second_piece[0] == "-" else (
+                                (-1, 1, 0) if second_piece[0] == "\\" else
+                                (-1, -1, 0)
+                            )
+            
+        elif second_piece[-1] in self.position_chars:
+            second_color = second_piece[0]
+            second_name = second_piece[1:-1]
+            second_position = self.get_piece(second_name, second_color).get_position()
+            position_change = (2, 0, 0) if second_piece[-1] == "-" else (
+                                (1, -1, 0) if second_piece[-1] == "\\" else
+                                (1, 1, 0)
+                            )
+            
+        else:
+            second_color = second_piece[0]
+            second_name = second_piece[1:]
+            second_position = self.get_piece(second_name, second_color).get_position()
+            position_change = (0, 0, 1)
+            
+        new_position = (second_position[0] + position_change[0], second_position[1] + position_change[1], second_position[2] + position_change[2])
+        first_piece.change_position(new_position)
+        
     
     def __str__(self) -> str:
         to_ret: str = "["
@@ -56,19 +101,23 @@ class Converter:
             
         for game_move, label in zip(game_moves, labels):
             game_move = game_move.split()
-            moves = [game_move[0]]
+            collector: Collector = Collector(game_move[0])
             i: int = 1
             while True:
-                self.from_mzinga_to_board(moves)
                 if game_move[i] == "pass":
-                    moves.append("pass")
                     i += 1
                 else:
-                    moves.append(game_move[i] + " " + game_move[i+1])
+                    new_move = game_move[i] + " " + game_move[i+1]
+                    collector.manage_piece(new_move)
+                    self.dump_string(collector, label)
                     i += 2
                 if i >= len(game_move):
                     break
                 
-    def from_mzinga_to_board(self, moves: list[str]):
-        collector: Collector = Collector()
-        # TODO
+    def dump_string(self, collector: Collector, label: str):
+        with open(self.result_file, "a") as file:
+            file.write(str(collector) + label)
+            
+if __name__ == "__main__":
+    converter = Converter("game_moves.txt", "labels.txt", "train_set.txt")
+    converter.convert()
