25aeea0779bcf3f4c15d155d9f416d813b0d3472
FGaragnani <filippogaragnani1@gmail.com>
2025-04-18 14:29:56 +0200

    Update logic.c

--- src/logic.c
+++ src/logic.c
@@ -28,6 +28,8 @@ char howManyAround(Context_t* context, Pieces_t piece) {
 		if (pieceId == piece)
 			continue;
 		Position_t* curPos = &idToPos[pieceId];
+		if (curPos->x == -1)
+			continue;
 		if (curPos->z == idToPos[piece].z) {
 			if (distance(curPos, &idToPos[piece]) == 2)
 				nearAround++;
@@ -48,8 +50,8 @@ evaluate = (
     1.5 * opponent_pressure_on_own_queen +        ! # Threats you're facing
     2.0 * own_beetle_on_opponent_queen +          ! # Climbing the enemy queen is very strong
     1.0 * own_ant_freedom +                       ! # Ants are powerful if free
-    0.8 * own_piece_count -						  !
-    0.5 * opponent_piece_count                    !
+    0.8 * own_piece_count -						  
+    0.5 * opponent_piece_count                    
 )
 */
 
@@ -66,11 +68,27 @@ int32_t heuristicValue(Context_t *context, bool areWeWhite) {
 		return 0;
 
 	int32_t value = 0;
+	char ourPieces, displaced; ourPieces = displaced = 0;
+	Position_t *idToPos = context->idToPos;
+	for (size_t pieceId = 0; pieceId < NUM_PIECES; pieceId++) {
+		if (idToPos[pieceId].x == -1)
+			continue;
+		displaced++;
+		if (!areWeWhite && pieceId <= 13)
+			ourPieces++;
+		else if (areWeWhite && pieceId > 13)
+			ourPieces++;
+	}
+
 
 	// opponent_queen_surround_ratio
 	value += (12 * howManyAround(context, areWeWhite ? B_QUEEN : W_QUEEN));
 	// own_queen_surround_ratio
 	value -= (10 * howManyAround(context, areWeWhite ? W_QUEEN : B_QUEEN));
+	// own_piece_count
+	value += (int)(0.8 * ourPieces);
+	// opponent_piece_count 
+	value -= (int)(0.5 * (displaced - ourPieces));
 
 	return value;
 }
