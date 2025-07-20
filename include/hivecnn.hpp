c026abc058322bbe67c0bef78492f53ff2822401
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-07-20 10:41:02 +0200

    checkpoint model + updated model

--- include/hivecnn.hpp
+++ include/hivecnn.hpp
@@ -39,34 +39,40 @@ struct HiveCNNImpl : HiveNet
     {
         // Build conv layers
         conv_layers = register_module("conv_layers", torch::nn::Sequential(
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(18, 24, /*kernel_size=*/2).padding(1).bias(true)),
+                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(18, 32, /*kernel_size=*/3).padding(1)),
                                           torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)),
+                                          torch::nn::Functional(torch::leaky_relu),
 
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(24, 32, /*kernel_size=*/2).padding(1).bias(true)),
+                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, /*kernel_size=*/3).padding(1)),
                                           torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)),
+                                          torch::nn::Functional(torch::leaky_relu),
+                                          torch::nn::Dropout(0.2),
 
+                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, /*kernel_size=*/3).padding(1)),
+                                          torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)),
+                                          torch::nn::Functional(torch::leaky_relu),
+                                          torch::nn::Dropout(0.2),
 
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 48, /*kernel_size=*/2).padding(1).bias(true)),
-                                          torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2))
+                                            torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, /*kernel_size=*/3).stride(2).padding(1).bias(false)),
+                                            torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(256)),
+                                            torch::nn::Functional(torch::leaky_relu),
+                                            torch::nn::Dropout(0.3),
+
+                                            torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 512, /*kernel_size=*/2).stride(1).padding(1).bias(false)),
+                                            torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(512)),
+                                            torch::nn::Functional(torch::relu),
+                                            torch::nn::Dropout(0.3)
                                       ));
 
         // Fully connected layers
         fc_layers = register_module("fc_layers", torch::nn::Sequential(
 
-                                        torch::nn::Linear(288, 128),
-                                        torch::nn::Dropout(0.2),
+                                        torch::nn::Linear(512, 32),
                                         torch::nn::Functional(torch::relu),
-
-                                        torch::nn::Linear(128, 32),
                                         torch::nn::Dropout(0.2),
-                                        torch::nn::Functional(torch::relu),
 
-                                        torch::nn::Linear(32, 8),
-                                        torch::nn::Dropout(0.05),
-                                        torch::nn::Functional(torch::relu)
+                                        torch::nn::Linear(32, 1)
                                     ));
-
-        register_module("pool", pool);
     }
 
     torch::Tensor forward(const Context_t *context) override;
@@ -88,42 +94,43 @@ struct HiveCNNEnhancedImpl : HiveNet
         : checkpoint_file(std::move(checkpoint))
     {
         conv_layers = register_module("conv_layers", torch::nn::Sequential(
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(18, 32, /*kernel_size=*/3).stride(1).padding(1).bias(true)),
-                                          torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(32)),
-                                          torch::nn::ReLU(),
+            // First conv block (18 -> 32)
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(18, 32, 3).stride(1).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(32),
+            torch::nn::Functional(torch::leaky_relu),
 
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, /*kernel_size=*/3).stride(1).padding(1).bias(true)),
-                                          torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(64)),
-                                          torch::nn::ReLU(),
+            // Second conv block (32 -> 64)
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, 3).stride(1).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(64),
+            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::Dropout(0.2),
 
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, /*kernel_size=*/3).stride(1).padding(1).bias(true)),
-                                          torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(128)),
-                                          torch::nn::ReLU(),
+            // Third conv block (64 -> 128)
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, 3).stride(1).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(128),
+            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::Dropout(0.2),
 
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, /*kernel_size=*/3).stride(2).padding(1).bias(true)),
-                                          torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(256)),
-                                          torch::nn::ReLU(),
-                                          torch::nn::Dropout(0.1),
+            // Fourth conv block (128 -> 256)
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, 3).stride(2).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(256),
+            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::Dropout(0.3),
 
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 512, /*kernel_size=*/3).stride(2).padding(1).bias(true)),
-                                          torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(512)),
-                                          torch::nn::ReLU(),
-                                          torch::nn::Dropout(0.1),
-
-                                          torch::nn::Conv2d(torch::nn::Conv2dOptions(512, 128, /*kernel_size=*/3).stride(1).padding(1).bias(true)),
-                                          torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(128)),
-                                          torch::nn::ReLU(),
-
-                                          torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2))
-                                      ));
+            // Fifth conv block (256 -> 512)
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 512, 2).stride(1).padding(1).bias(false)),
+            torch::nn::BatchNorm2d(512),
+            torch::nn::Functional(torch::relu),
+            torch::nn::Dropout(0.3)
+        ));
 
         // Fully connected layers
         fc_layers = register_module("fc1", torch::nn::Sequential(
-                                        torch::nn::Linear(128, 16),
-                                        torch::nn::ReLU(),
-                                        torch::nn::Dropout(0.1),
-                                        torch::nn::Linear(16, 1)
-                                    ));
+            torch::nn::Linear(torch::nn::LinearOptions(512, 32).bias(false)),
+            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::Dropout(0.2),
+            torch::nn::Linear(torch::nn::LinearOptions(32, 1).bias(false))
+        ));
 
         // TODO: Add an action-prob layer?
     }
