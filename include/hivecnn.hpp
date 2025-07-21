c8e308624620839bef4f85a76b12761bfe2b1596
FGaragnani <filippogaragnani1@gmail.com>
2025-07-21 16:35:40 +0200

    fix hope

--- include/hivecnn.hpp
+++ include/hivecnn.hpp
@@ -41,26 +41,26 @@ struct HiveCNNImpl : HiveNet
         conv_layers = register_module("conv_layers", torch::nn::Sequential(
                                           torch::nn::Conv2d(torch::nn::Conv2dOptions(18, 32, /*kernel_size=*/3).padding(1)),
                                           torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)),
-                                          torch::nn::Functional(torch::leaky_relu),
+                                          torch::nn::LeakyReLU(),
 
                                           torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, /*kernel_size=*/3).padding(1)),
                                           torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)),
-                                          torch::nn::Functional(torch::leaky_relu),
+                                          torch::nn::LeakyReLU(),
                                           torch::nn::Dropout(0.2),
 
                                           torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, /*kernel_size=*/3).padding(1)),
                                           torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2)),
-                                          torch::nn::Functional(torch::leaky_relu),
+                                          torch::nn::LeakyReLU(),
                                           torch::nn::Dropout(0.2),
 
                                             torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, /*kernel_size=*/3).stride(2).padding(1).bias(false)),
                                             torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(256)),
-                                            torch::nn::Functional(torch::leaky_relu),
+                                            torch::nn::LeakyReLU(),
                                             torch::nn::Dropout(0.3),
 
                                             torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 512, /*kernel_size=*/2).stride(1).padding(1).bias(false)),
                                             torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(512)),
-                                            torch::nn::Functional(torch::relu),
+                                            torch::nn::ReLU(),
                                             torch::nn::Dropout(0.3)
                                       ));
 
@@ -68,7 +68,7 @@ struct HiveCNNImpl : HiveNet
         fc_layers = register_module("fc_layers", torch::nn::Sequential(
 
                                         torch::nn::Linear(512, 32),
-                                        torch::nn::Functional(torch::relu),
+                                        torch::nn::ReLU(),
                                         torch::nn::Dropout(0.2),
 
                                         torch::nn::Linear(32, 1)
@@ -97,37 +97,37 @@ struct HiveCNNEnhancedImpl : HiveNet
             // First conv block (18 -> 32)
             torch::nn::Conv2d(torch::nn::Conv2dOptions(18, 32, 3).stride(1).padding(1).bias(false)),
             torch::nn::BatchNorm2d(32),
-            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::LeakyReLU(),
 
             // Second conv block (32 -> 64)
             torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, 3).stride(1).padding(1).bias(false)),
             torch::nn::BatchNorm2d(64),
-            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::LeakyReLU(),
             torch::nn::Dropout(0.2),
 
             // Third conv block (64 -> 128)
             torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, 3).stride(1).padding(1).bias(false)),
             torch::nn::BatchNorm2d(128),
-            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::LeakyReLU(),
             torch::nn::Dropout(0.2),
 
             // Fourth conv block (128 -> 256)
             torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 256, 3).stride(2).padding(1).bias(false)),
             torch::nn::BatchNorm2d(256),
-            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::LeakyReLU(),
             torch::nn::Dropout(0.3),
 
             // Fifth conv block (256 -> 512)
             torch::nn::Conv2d(torch::nn::Conv2dOptions(256, 512, 2).stride(1).padding(1).bias(false)),
             torch::nn::BatchNorm2d(512),
-            torch::nn::Functional(torch::relu),
+            torch::nn::ReLU(),
             torch::nn::Dropout(0.3)
         ));
 
         // Fully connected layers
         fc_layers = register_module("fc1", torch::nn::Sequential(
             torch::nn::Linear(torch::nn::LinearOptions(512, 32).bias(false)),
-            torch::nn::Functional(torch::leaky_relu),
+            torch::nn::LeakyReLU(),
             torch::nn::Dropout(0.2),
             torch::nn::Linear(torch::nn::LinearOptions(32, 1).bias(false))
         ));
